4abfc7b76661c31a1ba361bd4ee802df671491b1
Vancoetsem Geoffrey <ugeva@crdom001.prive.cerp-rouen.fr>
2018-06-18 17:14:29 +0200

    Ajout de service de collection et d'items

---
+++ my-app/src/app/core/services/collection.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Item } from '../../shared/interfaces/item';
+import { COLLECTION } from '../collection';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CollectionService {
+  private _collection: Item[];
+  constructor() {
+    this.collection = COLLECTION;
+  }
+
+  /**
+   * get collection
+   */
+  public get collection(): Item[] {
+    return this._collection;
+  }
+
+  /**
+   * set collection
+   */
+  public set collection(col: Item[]) {
+    this._collection = col;
+  }
+
+  /**
+   * get item from collection
+   */
+
+  /**
+   * update item in collection
+   */
+
+  /**
+   * delete item in collection
+   */
+}
